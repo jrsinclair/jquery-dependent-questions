f02d460ef279b58f3279acc89428eefe4dc9b374
James Sinclair <jsinclair@squiz.com.au>
2012-12-20 12:45:22 +1100

    Added support for checkboxes and pre-set values
    Added code to handle the case where a dependent question should be shown on page load and only hidden on change.
    
    As part of this, also added code for handling the values returned from checkboxes.

--- jquery.dependent-questions.js
+++ jquery.dependent-questions.js
@@ -18,6 +18,35 @@
     // See http://bit.ly/rc0Nzl for reasoning on why window, document and undefined
     // are passed in here.
 
+
+    // The some() method is not implemented in some browsers.
+    // See https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/some
+    // for the origin of this polyfill.
+    if (!Array.prototype.some) {
+        Array.prototype.some = function (fun, thisp) {
+            var t, len, i;
+            if (this === null) { throw new TypeError(); }
+
+            t   = Object(this);
+            len = t.length >>> 0;
+            if (typeof fun !== "function") { throw new TypeError(); }
+
+            for (i = 0; i < len; i += 1) {
+                if (i in t && fun.call(thisp, t[i], i, t)) { return true; }
+            }
+            return false;
+        };
+    }
+
+    // Return true if haystack contains needle 
+    function contains(haystack, needle) {
+        function equals(haystrand) {
+            return haystrand === needle;
+        }
+        return haystack.some(equals);
+    }
+
+
     // Configuration and defaults.
     var pluginName = 'dependentQuestions',
         defaults = {
@@ -52,7 +81,7 @@
     // listeners.
     QuestionToggler.prototype.init = function () {
         // Find all elements that have a 'data-depends-on' attribute.
-        var toHide, dataRe, dependentsMap, effect, duration;
+        var questions, dataRe, dependentsMap, effect, duration;
 
         effect        = this.options.effect;
         duration      = this.options.duration;
@@ -63,15 +92,41 @@
 
         // Fetch all the elements with 'data-depends-on' attributes.
         if (!this.element.find) { this.element = $(this.element); }
-        toHide = this.element.find('[data-depends-on]');
+        questions = this.element.find('[data-depends-on]');
+
+        // Get the value of a form element by name, even if it is a radio button
+        // or checkbox
+        function getValueByName(inputName) {
+            var multiInputs, el, elType, multi, modifier, checkVals;
+            multiInputs = ['radio', 'checkbox'];
+            el = $('[name="' + inputName + '"]');
+            if (el.length === 0) { return undefined; }
+
+            elType = el.attr('type');
+            multi  = contains(multiInputs, elType);
+            if (!multi) { return el.val(); }
+
+            if (elType === 'radio') { return el.filter(':checked').val(); }
+
+            if (elType === 'checkbox') {
+                checkVals = $.map(el.filter(':checked'), function (checkbox) {
+                    return $(checkbox).val();
+                });
+                return checkVals;
+            }
+        }
 
         // Event handler for when a toggling element changes value.
         function onTogglerChange(evt) {
-            var toggler, dependents, showVal, effectFunc, action;
+            var toggler, dependents, showVal, val, show, effectFunc, action;
             toggler    = $(evt.target);
             dependents = toggler.data('dependents');
             showVal    = toggler.data('showVal');
-            action     = (toggler.val() === showVal) ? 'show' : 'hide';
+            val        = getValueByName(toggler.attr('name'));
+            show       = (typeof val === 'object')
+                       ? contains(val, showVal)
+                       : (val === showVal);
+            action     = (show) ? 'show' : 'hide';
             effectFunc = effectsMap[effect][action];
             dependents[effectFunc](duration);
         }
@@ -87,7 +142,7 @@
                 dependentsMap[data] = el.add(dependentsMap[data]);
             }
         }
-        toHide.each(addMapEntry);
+        questions.each(addMapEntry);
 
         // Create event listeners
         function setListener(key, dependents) {
@@ -106,7 +161,27 @@
         }
         $.each(dependentsMap, setListener);
 
-        toHide.hide();
+        // Set the initial state of each dependent question to match form state.
+        function showHideByValue(key, dependents) {
+            var matches, name, showVal, toggler, val, func, show;
+            // Parse the key value
+            matches = dataRe.exec(key);
+            if (!matches) { return; }
+            name    = matches[1];
+            showVal = matches[2];
+
+            // Work out whether or not to show the value
+            toggler = $('[name="' + name + '"]');
+            val     = getValueByName(name);
+            if (typeof val === 'object') {
+                show = contains(val, showVal);
+            } else {
+                show = (val === showVal);
+            }
+            func = (show) ? 'show' : 'hide';
+            dependents[func]();
+        }
+        $.each(dependentsMap, showHideByValue);
     };
 
     // A really lightweight plugin wrapper around the constructor, 
